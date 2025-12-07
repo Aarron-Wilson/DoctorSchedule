1de2bb7209266c4ac0ca3fca9dc063517df66978
Aarron-Wilson <aarronwilson03@gmail.com>
2025-12-07 00:51:49 +0000

    Lots of features

--- Main/views.py
+++ Main/views.py
@@ -1,42 +1,57 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .models import *
 from .forms import *
 from django.http import JsonResponse
 
 
 def home(request):
-    return render(request,'home.html')
+    return render(request, 'home.html')
+
 
 def roster_page(request):
     doctors = Doctor.objects.all().order_by('name')
-    return render(request,'roster.html',{'doctors':doctors})
+    return render(request, 'roster.html', {'doctors': doctors})
+
 
 def block_page(request):
     all_blocks = Section.objects.all()
-    return render(request,'block.html',{'blocks':all_blocks})
+    return render(request, 'block.html', {'blocks': all_blocks})
+
 
 def schedule_page(request):
-    return render(request,'schedule.html')
+    return render(request, 'schedule.html')
 
-def create_edit_block(request,pk=None):
+
+def create_edit_block(request, pk=None):
     if request.method == 'POST':
         if pk:
-            form = BlockForm(request.POST,instance=Section.objects.get(pk=pk))
+            form = BlockForm(request.POST, instance=Section.objects.get(pk=pk))
         else:
             form = BlockForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('/block')
         else:
-            return render(request, 'myapp/includes/create_item_full_modal.html', {'form': form}, status=400)
+            return render(request,
+                          'myapp/includes/create_item_full_modal.html',
+                          {'form': form},
+                          status=400)
     else:
         if pk:
             form = BlockForm(instance=Section.objects.get(pk=pk))
         else:
             form = BlockForm()
-        
-        return render(request,'create_edit_block.html',{'form':form,'pk':pk})
 
-def delete_block(request,pk):
+        return render(request, 'create_edit_block.html', {
+            'form': form,
+            'pk': pk
+        })
+
+def delete_block(request, pk):
     Section.objects.get(pk=pk).delete()
     return JsonResponse({"message": "Success"})
+
+def manage_job_slots(request,pk):
+    block = Section.objects.get(pk=pk)
+    job_slots = JobSlot.objects.filter(section=block)
+    return render(request, 'manage_job_slots.html', {'block': block,'job_slots':job_slots})
